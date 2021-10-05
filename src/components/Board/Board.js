2a9d71fe20b896ac484b9f662b06fae25bb7f75a
Abhishek Narkhede <55378666+AbhishekN7@users.noreply.github.com>
2021-10-05 14:39:30 +0530

    changes

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -6,13 +6,13 @@ export const Board = () => {
     return (
             <div id="board">
                 <Cell value="X"/>
-                <Cell value="X"/>
-                <Cell value="X"/>
                 <Cell value=""/>
-                <Cell value="O"/>
-                <Cell value="X"/>
-                <Cell value="O"/>
-                <Cell value="X"/>
+                <Cell value=""/>
+                <Cell value=""/>
+                <Cell value=""/>
+                <Cell value=""/>
+                <Cell value=""/>
+                <Cell value=""/>
                 <Cell value="O"/>
             </div>
     )
